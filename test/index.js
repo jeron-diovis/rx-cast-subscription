66c12f3e8fdbbf8102fe670f663a96e90c310d97
Andrey Bobukh <andrey.bobukh@mediasapiens.co>
2016-04-13 17:27:32 +0300

    upd. tests for rx

--- test/index.js
+++ test/index.js
@@ -20,28 +20,41 @@ describe("rx", () => {
     subscribe = null;
   });
 
-  it("values and errors", () => {
+  it("values", () => {
     const onValue = sinon.spy();
-    const onError = sinon.spy();
 
     assert.isFunction(subscribe, "subscribe is not a function");
 
-    const unsubscribe = subscribe(onValue, onError);
+    const unsubscribe = subscribe(onValue);
     assert.isFunction(unsubscribe, "unsubscribe is not a function");
 
     stream.onNext();
-    stream.onError();
-
-    assert.equal(onValue.callCount, 1, "value subscription does not work");
-    assert.equal(onError.callCount, 1, "error subscription does not work");
+    assert.equal(onValue.callCount, 1, "subscription does not work");
 
     unsubscribe();
 
     stream.onNext();
-    stream.onError();
+    assert.equal(onValue.callCount, 1, "subscription is no cleared");
+  });
 
-    assert.equal(onValue.callCount, 1, "value subscription is not cleared");
-    assert.equal(onError.callCount, 1, "error subscription is not cleared");
+  it("errors", () => {
+    const onError = sinon.spy();
+
+    assert.isFunction(subscribe, "subscribe is not a function");
+
+    stream.catch();
+
+    const unsubscribe = subscribe(undefined, onError);
+    assert.isFunction(unsubscribe, "unsubscribe is not a function");
+
+    stream.onError(1);
+    assert.equal(onError.callCount, 1, "subscription does not work");
+
+    // Don't test `unsubscribe`, because (as far as I understood)
+    // in Rx uncaught error just ends stream,
+    // and caught error is not passed to `onError` listener.
+    // That is, there is no way for `onError` listener to be called twice.
+    // Someone correct me if I'm wrong.
   });
 
   it("end", () => {
@@ -49,7 +62,7 @@ describe("rx", () => {
     subscribe(undefined, undefined, onEnd);
     stream.onCompleted();
     stream.onCompleted();
-    assert.equal(onEnd.callCount, 1, "end subscription does not work");
+    assert.equal(onEnd.callCount, 1, "subscription does not work");
   });
 });
 
@@ -157,23 +170,26 @@ describe("bacon", () => {
 
 describe("most", () => {
   it("values", () => {
-    const observer = sinon.spy();
+    const onValue = sinon.spy();
 
     const { stream, observer: subject } = MostSubject();
 
     const subscribe = cast(stream);
     assert.isFunction(subscribe, "subscribe is not a function");
 
-    const unsubscribe = subscribe(observer);
+    const unsubscribe = subscribe(onValue);
     assert.isFunction(unsubscribe, "unsubscribe is not a function");
 
     subject.next();
+    assert.equal(onValue.callCount, 1, "subscription does not work");
+
     unsubscribe();
+
     subject.next();
 
     // because can't unsubscribe
     // @link https://github.com/cujojs/most/issues/142
-    assert.equal(observer.callCount, 2, "subscription cleared? how?");
+    assert.equal(onValue.callCount, 2, "subscription cleared? how?");
   });
 
   it("errors", () => {
